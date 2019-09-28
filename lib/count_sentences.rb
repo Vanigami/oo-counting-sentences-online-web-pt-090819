b8facf4b8ba18334b1256caffa491c5597f45ac0
Vanna Winland <vgwinland@gmail.com>
2019-09-28 01:47:53 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
   
   def count_sentences
-  count = self.split(/\.|\?|\!/).delete_if {|i| i.size > 2}.size
+  count = self.split(/\.|\?|\!/).delete_if {|i| i.length > 2}.length
   return count.length
   end
 end
